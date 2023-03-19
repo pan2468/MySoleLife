6627586e3a1b7e23ce34eb9ca3ed3324fdbd586a
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-03-19 10:58:46 +0900

    스프래시 화면 만들기

---
+++ app/src/main/java/com/chaeseong/mysolelife/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.chaeseong.mysolelife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chaeseong.mysolelife.R
+
+class IntroActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_intro)
+	}
+}
