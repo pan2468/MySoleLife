01a3def8b00a2a2536228547bd30102873a698c5
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-03-19 13:13:18 +0900

    로그인, 회원가입 페이지 xml 구현

--- app/src/main/java/com/chaeseong/mysolelife/auth/IntroActivity.kt
+++ app/src/main/java/com/chaeseong/mysolelife/auth/IntroActivity.kt
@@ -1,12 +1,30 @@
 package com.chaeseong.mysolelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.chaeseong.mysolelife.R
+import com.chaeseong.mysolelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityIntroBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
+
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_intro)
+
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+		binding.loginBtn.setOnClickListener {
+			val intent = Intent(this, LoginActivity::class.java)
+			startActivity(intent)
+		}
+
+		binding.joinBtn.setOnClickListener {
+			val intent = Intent(this, JoinActivity::class.java)
+			startActivity(intent)
+		}
 	}
 }
