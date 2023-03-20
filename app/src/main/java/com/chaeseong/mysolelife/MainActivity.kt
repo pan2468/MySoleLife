08ec501bf0ebc9189b61834a7b4e25142c4dcfb7
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-03-20 22:58:50 +0900

    익명 로그인, 로그인, 로그아웃

--- app/src/main/java/com/chaeseong/mysolelife/MainActivity.kt
+++ app/src/main/java/com/chaeseong/mysolelife/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.chaeseong.mysolelife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.chaeseong.mysolelife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+	private lateinit var auth: FirebaseAuth
+
 	override fun onCreate(savedInstanceState: Bundle?) {
+
+		auth = FirebaseAuth.getInstance()
+
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+			auth.signOut()
+
+			val intent = Intent(this, IntroActivity::class.java)
+			intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+			startActivity(intent)
+
+		}
+
 	}
 }
--- app/src/main/java/com/chaeseong/mysolelife/auth/IntroActivity.kt
+++ app/src/main/java/com/chaeseong/mysolelife/auth/IntroActivity.kt
@@ -3,11 +3,16 @@ package com.chaeseong.mysolelife.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.chaeseong.mysolelife.MainActivity
 import com.chaeseong.mysolelife.R
 import com.chaeseong.mysolelife.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class IntroActivity : AppCompatActivity() {
+
+	private lateinit var auth: FirebaseAuth
 
 	private lateinit var binding: ActivityIntroBinding
 
@@ -15,6 +20,7 @@ class IntroActivity : AppCompatActivity() {
 
 		super.onCreate(savedInstanceState)
 
+		auth = FirebaseAuth.getInstance()
 		binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
 		binding.loginBtn.setOnClickListener {
@@ -25,6 +31,25 @@ class IntroActivity : AppCompatActivity() {
 		binding.joinBtn.setOnClickListener {
 			val intent = Intent(this, JoinActivity::class.java)
 			startActivity(intent)
+		}
+
+		binding.noAccountBtn.setOnClickListener {
+			auth.signInAnonymously()
+				.addOnCompleteListener(this) { task ->
+					if (task.isSuccessful) {
+
+						Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+						val intent = Intent(this, MainActivity::class.java)
+						intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+						startActivity(intent)
+
+					} else {
+
+						Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+
+					}
+				}
 		}
 	}
 }
