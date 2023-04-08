0a3fbf51028fbfd7d255f33afa7f7ee9549970e1
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-04-08 17:36:36 +0900

    컨텐츠 리스트 만들기 - RecyclerView Item Click

--- app/src/main/java/com/chaeseong/mysolelife/contentsList/ContentModel.kt
+++ app/src/main/java/com/chaeseong/mysolelife/contentsList/ContentModel.kt
@@ -2,5 +2,6 @@ package com.chaeseong.mysolelife.contentsList
 
 data class ContentModel(
 	var title : String = "",
-	var imageUrl : String = ""
+	var imageUrl : String = "",
+	var webUrl : String = ""
 )
--- app/src/main/java/com/chaeseong/mysolelife/contentsList/ContentRvAdapter.kt
+++ app/src/main/java/com/chaeseong/mysolelife/contentsList/ContentRvAdapter.kt
@@ -12,12 +12,24 @@ import com.chaeseong.mysolelife.R
 
 class ContentRvAdapter(val context: Context, val items: ArrayList<ContentModel>): RecyclerView.Adapter<ContentRvAdapter.Viewholder>() {
 
+	interface ItemClick{
+		fun onClick(view: View, position: Int)
+	}
+	var itemClick: ItemClick? = null
+
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentRvAdapter.Viewholder {
 		val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
 		return Viewholder(v)
 	}
 
 	override fun onBindViewHolder(holder: ContentRvAdapter.Viewholder, position: Int) {
+
+		if(itemClick != null){
+			holder.itemView.setOnClickListener { v ->
+				itemClick?.onClick(v, position);
+			}
+		}
+
 		holder.bindItems(items[position])
 	}
 
